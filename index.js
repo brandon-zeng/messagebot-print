a4b69e5383e07ebc4fd3ab41cdc8cda7c7cab769
Wenjuan Zhang <wenjuanzhang@MacBook-Pro.local>
2017-10-22 15:58:04 -0400

    add logging and port

--- index.js
+++ index.js
@@ -1,23 +1,16 @@
 'use strict';
 
 // Imports dependencies and set up http server
-const 
+const
   fs = require('fs'),
   https = require('https'),
   express = require('express'),
   bodyParser = require('body-parser'),
   app = express().use(bodyParser.json()); // creates express http server
 
-// Sets server port and logs message on success
-// app.listen(process.env.PORT || 1337, () => console.log('webhook is listening'));
-https.createServer({
-      key: fs.readFileSync('auth/key.pem'),
-      cert: fs.readFileSync('auth/cert.pem'),
-      passphrase: 'abcde'
-    }, app).listen(5555);
+app.set('port', (process.env.PORT || 5555));
 
-
-app.get('/test', (req, res) => {
+app.get('/ping', (req, res) => {
   res.status(200).send('{"message": "welcome"}');
 });
 
@@ -27,39 +20,39 @@ app.get('/webhook', (req, res) => {
   console.log('receive /webhook get request');
   // Your verify token. Should be a random string.
   let VERIFY_TOKEN = "EAACouNekzsIBAKSbVILZBAHQwSlW7ZBoIaLiztK1znRkzOhJozYRp1NEfXfbXnC26xkFdhHVO1wvogZAayAGATvZAghuqpD17ylDDBH0JZC6OcADtRBbzQ5eT0uF3HZAE1bYzvGl5TqBFDoAz5nsmDCo1GEZB3IU2yQdzQ0jyhuuwZDZD"
-    
+
   // Parse the query params
   let mode = req.query['hub.mode'];
   let token = req.query['hub.verify_token'];
   let challenge = req.query['hub.challenge'];
-    
+
   // Checks if a token and mode is in the query string of the request
   if (mode && token) {
-  
+
     // Checks the mode and token sent is correct
     if (mode === 'subscribe' && token === VERIFY_TOKEN) {
-      
+
       // Responds with the challenge token from the request
       console.log('WEBHOOK_VERIFIED');
       res.status(200).send(challenge);
-    
+
     }
-  }  else {
-      // Responds with '403 Forbidden' if verify tokens do not match
-      res.sendStatus(403);      
-    }
+  } else {
+    // Responds with '403 Forbidden' if verify tokens do not match
+    res.sendStatus(403);
+  }
 });
 
 // Creates the endpoint for our webhook 
-app.post('/webhook', (req, res) => {  
- 
+app.post('/webhook', (req, res) => {
+
   let body = req.body;
 
   // Checks this is an event from a page subscription
   if (body.object === 'page') {
 
     // Iterates over each entry - there may be multiple if batched
-    body.entry.forEach(function(entry) {
+    body.entry.forEach(function (entry) {
 
       // Gets the message. entry.messaging is an array, but 
       // will only ever contain one message, so we get index 0
@@ -74,4 +67,15 @@ app.post('/webhook', (req, res) => {
     res.sendStatus(404);
   }
 
+});
+
+
+// Sets server port and logs message on success
+// app.listen(process.env.PORT || 1337, () => console.log('webhook is listening'));
+https.createServer({
+  key: fs.readFileSync('auth/key.pem'),
+  cert: fs.readFileSync('auth/cert.pem'),
+  passphrase: 'abcde'
+}, app).listen(app.get('port'), function () {
+  console.log('Example app listening on port' + app.get('port'));
 });
